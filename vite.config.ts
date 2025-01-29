be57380ebde382dc04187d5ba6dbff4ed834ab6a
Tarachand <tarachand90@gmail.com>
2025-01-29 23:23:16 +0530

    Change the default port 5173 to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, //change the default port to 3000
+  }
 })
